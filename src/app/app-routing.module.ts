a1cdfcc13335b0c04bcce3b7f9fcf79b35dcfd09
Javier Aranda <javierarandalopez96@gmail.com>
2024-06-04 21:03:19 +0200

    my first lazy load routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesRoutingModule } from './countries/countries.routing.module';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path:"contact",
     component:ContactPageComponent
+  },
+  {
+    path:"countries",
+    loadChildren:() => import ('./countries/countries.module').then(m => m.CountriesModule)
   },
   {
     path:"**",
